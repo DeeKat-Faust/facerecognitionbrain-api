504407af2b5bd40b5969b7420384445720d30ab3
DeeKat <deepak.281295@gmail.com>
2021-05-12 02:44:11 +0530

    resolved signup collection error

--- controllers/Register.js
+++ controllers/Register.js
@@ -1,44 +1,57 @@
-const handleRegister = (req, res, usersCollection, bcrypt) => {
-    const { email, name, password } = req.body;
+const handleRegister = (
+  req,
+  res,
+  signUpCollection,
+  usersCollection,
+  bcrypt
+) => {
+  const { email, name, password } = req.body;
 
-    usersCollection.findOne({ email: email })
-        .then((value) => {
-            if (value) {
-                res.json('Email is already in use');
+  usersCollection
+    .findOne({ email: email })
+    .then((value) => {
+      if (value) {
+        res.json("Email is already in use");
+      } else {
+        usersCollection.insertOne(
+          {
+            email: email,
+            name: name,
+            entries: 0,
+            joined: new Date(),
+          },
+          (err, info) => {
+            if (err) {
+              console.log(err);
+              res.json("Error registering user!");
             } else {
-                usersCollection.insertOne({
-                    email: email,
-                    name: name,
-                    entries: 0,
-                    joined: new Date()
-                }, (err, info) => {
-                    if (err) {
-                        console.log(err);
-                        res.json('Error registering user!');
-                    } else {
-                        // hash the password
-                        const salt = bcrypt.genSaltSync(10);
-                        const hash = bcrypt.hashSync(password, salt);
-                        signUpCollection.insertOne({
-                            email: email,
-                            hash: hash
-                        }, (err, user) => {
-                            if (err) {
-                                console.log(err);
-                                res.json('Error registering user!');
-                            } else {
-                                res.json(info.ops[0]);
-                            }
-                        });
-                    }
-                });
+              // hash the password
+              const salt = bcrypt.genSaltSync(10);
+              const hash = bcrypt.hashSync(password, salt);
+              signUpCollection.insertOne(
+                {
+                  email: email,
+                  hash: hash,
+                },
+                (err, user) => {
+                  if (err) {
+                    console.log(err);
+                    res.json("Error registering user!");
+                  } else {
+                    res.json(info.ops[0]);
+                  }
+                }
+              );
             }
-        })
-        .catch((err) => {
-            res.json('Error registering user!');
-        })
-}
+          }
+        );
+      }
+    })
+    .catch((err) => {
+      res.json("Error registering user!");
+    });
+};
 
 module.exports = {
-    handleRegister
-}
+  handleRegister,
+};
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require("express");
 const mongodb = require("mongodb");
-const dotenv = require('dotenv');
-const bcrypt = require('bcrypt');
-const cors = require('cors');
+const dotenv = require("dotenv");
+const bcrypt = require("bcrypt");
+const cors = require("cors");
 
-const GetUsers = require('./controllers/GetUsers');
-const Register = require('./controllers/Register');
-const Signin = require('./controllers/Signin');
-const Image = require('./controllers/Image');
-const DeleteUser = require('./controllers/DeleteUser');
+const GetUsers = require("./controllers/GetUsers");
+const Register = require("./controllers/Register");
+const Signin = require("./controllers/Signin");
+const Image = require("./controllers/Image");
+const DeleteUser = require("./controllers/DeleteUser");
 
 dotenv.config();
 
@@ -17,7 +17,7 @@ let usersCollection;
 let signUpCollection;
 let port = process.env.PORT;
 if (port == null || port == "") {
-    port = 3005;
+  port = 3005;
 }
 
 const app = express();
@@ -28,21 +28,33 @@ app.use(cors());
 
 const connectionString = process.env.CONNECTION_STRING;
 
-mongodb.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then((mongoClient) => {
-        console.log("Database connected successfully");
-        db = mongoClient.db();
-        usersCollection = db.collection('users');
-        signUpCollection = db.collection('signup');
-        app.listen(port);
-    })
-    .catch((reason) => {
-        console.log(reason);
-    });
+mongodb
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((mongoClient) => {
+    console.log("Database connected successfully");
+    db = mongoClient.db();
+    usersCollection = db.collection("users");
+    signUpCollection = db.collection("signup");
+    console.log(usersCollection);
+    console.log(signUpCollection);
+    app.listen(port);
+  })
+  .catch((reason) => {
+    console.log(reason);
+  });
 
-app.get('/', (req, res) => GetUsers.handleGetUsers(req, res, usersCollection));
-app.post('/register', (req, res) => Register.handleRegister(req, res, usersCollection, bcrypt));
-app.post('/signin', (req, res) => Signin.handleSignin(req, res, signUpCollection, usersCollection, bcrypt));
-app.put('/image', (req, res) => Image.handleImage(req, res, usersCollection));
-app.post('/imageurl', (req, res) => Image.handleAPICall(req, res));
-app.delete('/delete', (req, res) => DeleteUser.handleDeleteUser(req, res, usersCollection, signUpCollection));
+app.get("/", (req, res) => GetUsers.handleGetUsers(req, res, usersCollection));
+app.post("/register", (req, res) =>
+  Register.handleRegister(req, res, signUpCollection, usersCollection, bcrypt)
+);
+app.post("/signin", (req, res) =>
+  Signin.handleSignin(req, res, signUpCollection, usersCollection, bcrypt)
+);
+app.put("/image", (req, res) => Image.handleImage(req, res, usersCollection));
+app.post("/imageurl", (req, res) => Image.handleAPICall(req, res));
+app.delete("/delete", (req, res) =>
+  DeleteUser.handleDeleteUser(req, res, usersCollection, signUpCollection)
+);
